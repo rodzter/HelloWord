c53f684ad6dda7e326f07090225e5a1b04b4b235
rodzter <rod_campos1@hotmail.com>
2015-06-29 16:07:41 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HelloWord::Application.config.secret_key_base = '083e44e03757d128912154a81b1feb953fdf0d1a11ae37d4ab4bf1f064b5f4f1e36d8bb5c7dfe6d214e3d18afa723b014fa42a2790c0fa54e676ad03bc69c6a3'
